9933fb1831243acabbe23b03aa7613168f5653a8
Skrill <ivan.samal@student.ehu.lt>
2026-09-14 16:07:21 +0300

    Initial commit

---
+++ src/test/java/com/skr1l/minierp/MiniErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr1l.minierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
